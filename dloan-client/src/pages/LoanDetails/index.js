cb4e75b3e584cae5aece1be0aa05a14ac3d97fb3
Evan Edwin Lopez <elopez@iripple.com>
2023-04-22 21:33:18 +0800

    Added navigation to view detail route

--- dloan-client/src/pages/LoanDetails/index.js
+++ dloan-client/src/pages/LoanDetails/index.js
@@ -5,11 +5,14 @@ import { Content } from 'antd/es/layout/layout.js';
 import { contentStyle, headerStyle, headerText } from './styles.js';
 import DetailsForm from '../../components/Form/index.js';
 import { applicationId } from '../../api/services.js';
+import { useNavigate } from 'react-router-dom';
+import { Route as RouteName } from '../../navigation/Routes.js';
 
 const { Header, Footer } = Layout;
 
 const LoanDetails = () => {
   const [appToken, setAppToken] = useState('');
+  const navigate = useNavigate();
 
   useEffect(() => {
     applicationId()
@@ -19,6 +22,18 @@ const LoanDetails = () => {
       });
   });
 
+  const requestBalanceSheet = (form) => {
+    const data = {
+      tokenId: appToken,
+      accountingProvider: form.accountingProvider,
+      businessName: form.businessName,
+      startYear: form.yearEstablished,
+      loanAmount: form.loanAmount
+    };
+
+    navigate(RouteName.viewDetails, { state: { data } });
+  };
+
   return appToken ? (
     <Space direction="vertical" style={{ width: '100%', height: '100%' }} size={[0, 64]}>
       <Layout>
@@ -26,7 +41,7 @@ const LoanDetails = () => {
           <div style={headerText}>Get your business loan now!</div>
         </Header>
         <Content style={contentStyle}>
-          <DetailsForm />
+          <DetailsForm onFinish={(form) => requestBalanceSheet(form)} />
         </Content>
         <Footer style={footerStyle}>
           2023 All rights reserved. Development by Evan Edwin Lopez
