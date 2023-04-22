ad9afa634c4d3b6ba85035d600d3af94940d77e0
Evan Edwin Lopez <elopez@iripple.com>
2023-04-22 18:39:26 +0800

    Added loan details page

--- dloan-client/src/pages/LoanDetails/index.js
+++ dloan-client/src/pages/LoanDetails/index.js
@@ -1,22 +1,46 @@
-import React from 'react';
-import { Layout, Space } from 'antd';
-import { contentStyle, footerStyle, headerStyle, headerText } from '../LandingPage/styles.js';
+import React, { useEffect, useState } from 'react';
+import { Layout, Space, Spin } from 'antd';
+import { footerStyle } from '../LandingPage/styles.js';
 import { Content } from 'antd/es/layout/layout.js';
+import { contentStyle, headerStyle, headerText } from './styles.js';
+import DetailsForm from '../../components/Form/index.js';
+import { applicationId } from '../../api/services.js';
 
 const { Header, Footer } = Layout;
 
 const LoanDetails = () => {
-  return (
+  const [appToken, setAppToken] = useState('');
+
+  useEffect(() => {
+    applicationId()
+      .then((response) => response.json())
+      .then((data) => {
+        setAppToken(data.appId);
+      });
+  });
+
+  return appToken ? (
     <Space direction="vertical" style={{ width: '100%', height: '100%' }} size={[0, 64]}>
       <Layout>
         <Header style={headerStyle}>
           <div style={headerText}>Get your business loan now!</div>
         </Header>
-        <Content style={contentStyle}></Content>
+        <Content style={contentStyle}>
+          <DetailsForm />
+        </Content>
         <Footer style={footerStyle}>
           2023 All rights reserved. Development by Evan Edwin Lopez
         </Footer>
       </Layout>
+    </Space>
+  ) : (
+    <Space
+      direction="vertical"
+      style={{ width: '100%', height: '100%', justifyContent: 'center' }}
+      size={[0, 64]}>
+      <Spin tip="Loading" size="large">
+        <div className="content" />
+      </Spin>
     </Space>
   );
 };
