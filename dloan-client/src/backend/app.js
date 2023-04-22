92dd4ffb658812bde55fd516c4362c4b810255de
Evan Edwin Lopez <elopez@iripple.com>
2023-04-22 21:34:00 +0800

    Added parser on server config

--- dloan-client/src/backend/app.js
+++ dloan-client/src/backend/app.js
@@ -8,6 +8,8 @@ const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200
 };
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 app.use(indexRouter);
 
