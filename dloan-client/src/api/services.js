8fab99e70af963feeea08b34cc4386780f431350
Evan Edwin Lopez <elopez@iripple.com>
2023-04-22 21:31:09 +0800

    Added other service api's

--- dloan-client/src/api/services.js
+++ dloan-client/src/api/services.js
@@ -8,5 +8,21 @@ const options = {
 };
 
 export const applicationId = () => {
-  return fetch(`${BASE_URL}:${PORT}/initiateApplication`, options);
+  return fetch(`${BASE_URL}:${PORT}/initiateApplication`, { ...options });
+};
+
+export const getBalanceSheet = (data) => {
+  return fetch(`${BASE_URL}:${PORT}/balance-sheet`, {
+    ...options,
+    method: 'POST',
+    body: JSON.stringify(data)
+  });
+};
+
+export const getDecision = (data) => {
+  return fetch(`${BASE_URL}:${PORT}/decision-engine`, {
+    ...options,
+    method: 'POST',
+    body: JSON.stringify(data)
+  });
 };
